0e6a7b2f4503d479813ffb506350a542ae7c41c7
Olivier Brisse <ouranos@users.noreply.github.com>
2017-05-23 13:37:37 +1000

    [MNOE-456] Return new fields and shared entities

---
+++ core/lib/mno_enterprise/concerns/models/shared_entity.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Endpoint:
+#  - /v1/app/:app_id/shared_entities
+#
+#  id                :integer         not null, primary key
+#  nid               :string
+#  name              :string
+#  created_at        :datetime        not null
+#  updated_at        :datetime        not null
+
+module MnoEnterprise::Concerns::Models::SharedEntity
+  extend ActiveSupport::Concern
+b
+  #==================================================================
+  # Included methods
+  #==================================================================
+  included do
+    # == Relationships ========================================================
+    belongs_to :app #, class_name: 'MnoEnterprise::App'
+  end
+
+  #==================================================================
+  # Class methods
+  #==================================================================
+  module ClassMethods
+    # def some_class_method
+    #   'some text'
+    # end
+  end
+
+  #==================================================================
+  # Instance methods
+  #==================================================================
+
+  # == Constants ============================================================
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+
+
+
+  # == Validations ==========================================================
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
+end
---
+++ core/spec/models/mno_enterprise/shared_entity_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'shoulda/matchers'
+
+module MnoEnterprise
+  RSpec.describe SharedEntity, type: :model do
+    # it {is_expected.to belong_to(:app)}
+  end
+end
