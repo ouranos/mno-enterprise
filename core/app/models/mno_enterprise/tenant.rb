7a114077666d8a8767d329cd128f7704ef7a5e27
Olivier Brisse <ouranos@users.noreply.github.com>
2017-06-05 16:11:16 +1000

    Fetch config from mnohub

--- core/app/models/mno_enterprise/tenant.rb
+++ core/app/models/mno_enterprise/tenant.rb
@@ -1,7 +1,9 @@
 module MnoEnterprise
   class Tenant < BaseResource
+    attributes :frontend_config
+
     def self.show
-      self.get('tenant')
+      self.get('tenant').tap {|t| t.clear_attribute_changes!}
     end
   end
 end
